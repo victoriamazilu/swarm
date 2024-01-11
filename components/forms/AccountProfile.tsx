f2e80ab80fc5d958317789d0cc5e2a965d708442
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2024-01-11 17:36:48 -0500

    Update styling

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -27,10 +27,10 @@ const AccountProfile = ( { user, btnTitle }: Props ) => {
     const form = useForm({ 
         resolver: zodResolver(UserValidtion),
         defaultValues: {
-            profile_photo: "",
-            name: "",
-            username: "",
-            bio: "",
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username || "",
+            bio: user?.bio || "",
         }
     });
     
@@ -81,11 +81,11 @@ const AccountProfile = ( { user, btnTitle }: Props ) => {
                     control={form.control}
                     name="name"
                     render={({ field }) => (
-                        <FormItem className="flex items-center gap-4">
-                            <FormLabel className="text-base-semibold text-light-2 gap-3 w-full">
+                        <FormItem className="flex flex-col w-full gap-3">
+                            <FormLabel className="text-base-semibold text-light-2">
                                 Name
                             </FormLabel>
-                            <FormControl className="flex-1 text-base-semibold text-gray-200">
+                            <FormControl>
                                 <Input 
                                     type="text"
                                     className="account-form_input no-focus" 
@@ -101,8 +101,8 @@ const AccountProfile = ( { user, btnTitle }: Props ) => {
                     control={form.control}
                     name="username"
                     render={({ field }) => (
-                        <FormItem className="flex items-center gap-4">
-                            <FormLabel className="text-base-semibold text-light-2 gap-3 w-full">
+                        <FormItem className="flex flex-col w-full gap-3">
+                            <FormLabel className="text-base-semibold text-light-2">
                                 Username
                             </FormLabel>
                             <FormControl className="flex-1 text-base-semibold text-gray-200">
@@ -121,8 +121,8 @@ const AccountProfile = ( { user, btnTitle }: Props ) => {
                     control={form.control}
                     name="bio"
                     render={({ field }) => (
-                        <FormItem className="flex items-center gap-4">
-                            <FormLabel className="text-base-semibold text-light-2 gap-3 w-full">
+                        <FormItem className="flex flex-col w-full gap-3">
+                            <FormLabel className="text-base-semibold text-light-2">
                                 Bio
                             </FormLabel>
                             <FormControl className="flex-1 text-base-semibold text-gray-200">
