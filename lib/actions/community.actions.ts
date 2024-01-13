e5b75dfac17b1adb85432d523ab40a62ef013794
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2024-01-13 16:52:53 -0500

    Small updates

--- lib/actions/community.actions.ts
+++ lib/actions/community.actions.ts
@@ -2,7 +2,7 @@
 
 import { FilterQuery, SortOrder } from "mongoose";
 
-// import Community from "../models/community.model";
+import Community from "../models/community.model";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 
