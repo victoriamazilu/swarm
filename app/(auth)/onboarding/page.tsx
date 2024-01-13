bdc8504b7958f7d6741259eeaac9cccbf7c44fe0
victoriamazilu <138828561+victoriamazilu@users.noreply.github.com>
2024-01-12 19:56:35 -0500

    Updates

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,17 +1,23 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from '@clerk/nextjs';
+import { redirect } from "next/navigation";
 
 async function Page() {
     const user = await currentUser();
-    const userInfo = {};
-    const userData ={
-        id: user?.id,
-        objectId: userInfo?._id, 
-        username: userInfo?.username || user?.username,
-        name: userInfo?.name || user.firstName || "",
-        bio: userInfo?.bio || "",
-        image: userInfo?.image || user.imageURL,
-    }
+    if (!user) return null; // to avoid typescript warnings
+  
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
+  
+    const userData = {
+      id: user.id,
+      objectId: userInfo?._id,
+      username: userInfo ? userInfo?.username : user.username,
+      name: userInfo ? userInfo?.name : user.firstName ?? "",
+      bio: userInfo ? userInfo?.bio : "",
+      image: userInfo ? userInfo?.image : user.imageUrl,
+    };
 
     return(
         <main className="mx-auto flex mx-w-3xl flex-col justify-start px-10 py-20">
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -41,3 +41,29 @@ export async function createThread({
     }
 }
 
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    //calculate the number of posts to skip depending on the page number
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    //fetch the posts that have no parents (none of the comments)
+    const postsQuery = Thread.find({parentId: { $in: [null, undefined]}})
+        .sort({createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User}) //populate the creator
+        .populate({ path: 'children',  //populate the children
+            populate: { path: 'author', model: User, select: "_id name parentId image"}
+        })
+
+    //find total number of posts to figure out amount of pages --but only count the ones that are not comments
+    const totalPostsCount = await Thread.countDocuments({parentId: { $in: [null, undefined]},
+    });
+
+    const posts = await postsQuery.exec(); //execute the query
+
+    const isNext = totalPostsCount > (skipAmount + posts.length); //check if we need a next page
+
+    return { posts, isNext };
+}
