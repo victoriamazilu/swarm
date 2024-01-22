6da86a75b23434f08cb1dd88dab015687229a0d5
victoriamazilu <victoria.mazilu@icloud.com>
2024-01-21 20:27:38 -0500

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Nunito, Poppins} from "next/font/google";
+import { Poppins } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import "../globals.css";
@@ -9,36 +9,36 @@ import Bottombar from "@/components/shared/Bottombar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Topbar from "@/components/shared/Topbar";
 
-const font = Poppins({weight: "400", subsets: ["latin"]});
+const font = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Swarm",
-  description: "A Next.js Application for Advice, Discussion, and More",
+    title: "Swarm",
+    description: "A Next.js Application for Advice, Discussion, and More",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <ClerkProvider>
-      <html lang='en'>
-        <body className={font.className}>
-          <Topbar />
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={font.className}>
+                    <Topbar />
 
-          <main className='flex flex-row'>
-            <LeftSidebar />
-            <section className='main-container'>
-              <div className='w-full max-w-4xl'>{children}</div>
-            </section>
-            {/* @ts-ignore */}
-            <RightSidebar />
-          </main>
+                    <main className="flex flex-row">
+                        <LeftSidebar />
+                        <section className="main-container">
+                            <div className="w-full max-w-4xl">{children}</div>
+                        </section>
+                        {/* @ts-ignore */}
+                        <RightSidebar />
+                    </main>
 
-          <Bottombar />
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+                    <Bottombar />
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
