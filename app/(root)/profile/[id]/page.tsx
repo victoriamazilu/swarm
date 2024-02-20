da3b3aa581ff747168015e14acf4c330584e2abf
victoriamazilu <victoria.mazilu@icloud.com>
2024-02-20 09:41:32 -0500

    New feature
    Display buzzes for each profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -11,61 +11,67 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page({ params }: { params: { id: string } }) {
-  const user = await currentUser();
-  if (!user) return null;
+    const user = await currentUser();
+    if (!user) return null;
 
-  const userInfo = await fetchUser(params.id);
-  if (!userInfo?.onboarded) redirect("/onboarding");
+    const userInfo = await fetchUser(params.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
 
-  return (
-    <section>
-      <ProfileHeader
-        accountId={userInfo.id}
-        authUserId={user.id}
-        name={userInfo.name}
-        username={userInfo.username}
-        imgUrl={userInfo.image}
-        bio={userInfo.bio}
-      />
-
-      <div className='mt-9'>
-        <Tabs defaultValue='buzzes' className='w-full'>
-          <TabsList className='tab'>
-            {profileTabs.map((tab) => (
-              <TabsTrigger key={tab.label} value={tab.value} className='tab'>
-                <Image
-                  src={tab.icon}
-                  alt={tab.label}
-                  width={24}
-                  height={24}
-                  className='object-contain'
-                />
-                <p className='max-sm:hidden'>{tab.label}</p>
-
-                {tab.label === "Buzzes" && (
-                  <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-dark-2'>
-                    {userInfo.buzzes.length}
-                  </p>
-                )}
-              </TabsTrigger>
-            ))}
-          </TabsList>
-          {/* {profileTabs.map((tab) => (
-            <TabsContent
-              key={`content-${tab.label}`}
-              value={tab.value}
-              className='w-full text-dark-1'
-            >
-              <BuzzesTab
-                currentUserId={user.id}
+    return (
+        <section>
+            <ProfileHeader
                 accountId={userInfo.id}
-                accountType='User'
-              />
-            </TabsContent>
-          ))}  */}
-        </Tabs> 
-      </div>
-    </section>
-  );
+                authUserId={user.id}
+                name={userInfo.name}
+                username={userInfo.username}
+                imgUrl={userInfo.image}
+                bio={userInfo.bio}
+            />
+
+            <div className="mt-9">
+                <Tabs defaultValue="buzzes" className="w-full">
+                    <TabsList className="tab">
+                        {profileTabs.map((tab) => (
+                            <TabsTrigger
+                                key={tab.label}
+                                value={tab.value}
+                                className="tab"
+                            >
+                                <Image
+                                    src={tab.icon}
+                                    alt={tab.label}
+                                    width={24}
+                                    height={24}
+                                    className="object-contain"
+                                />
+                                <p className="max-sm:hidden">{tab.label}</p>
+
+                                {tab.label === "Buzzes" && (
+                                    <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-dark-2">
+                                        {userInfo.buzzes.length}
+                                    </p>
+                                )}
+                            </TabsTrigger>
+                        ))}
+                    </TabsList>
+                    {profileTabs.map((tab) =>
+                        tab.label === "Buzzes" ? (
+                            <TabsContent
+                                key={`content-${tab.label}`}
+                                value={tab.value}
+                                className="w-full text-dark-1"
+                            >
+                                <BuzzesTab
+                                    currentUserId={user.id}
+                                    accountId={userInfo.id}
+                                    accountType="User"
+                                />
+                            </TabsContent>
+                        ) : null
+                    )}
+                </Tabs>
+            </div>
+        </section>
+    );
 }
 export default Page;
