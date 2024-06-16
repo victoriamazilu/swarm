589614e408b854a6a5829fb1c79e343bdee99bc9
victoriamazilu <victoria.mazilu@icloud.com>
2024-06-16 14:52:41 -0400

    Bug fix w onboarding page
    Background wasn't covering entire screen. Also, prettify indentation.

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -5,33 +5,33 @@ import { fetchUser } from "@/lib/actions/user.actions";
 import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
-  const user = await currentUser();
-  if (!user) return null; // to avoid typescript warnings
+    const user = await currentUser();
+    if (!user) return null; // to avoid typescript warnings
 
-  const userInfo = await fetchUser(user.id);
-  if (userInfo?.onboarded) redirect("/");
+    const userInfo = await fetchUser(user.id);
+    if (userInfo?.onboarded) redirect("/");
 
-  const userData = {
-    id: user.id,
-    objectId: userInfo?._id,
-    username: userInfo ? userInfo?.username : user.username,
-    name: userInfo ? userInfo?.name : user.firstName ?? "",
-    bio: userInfo ? userInfo?.bio : "",
-    image: userInfo ? userInfo?.image : user.imageUrl,
-  };
+    const userData = {
+        id: user.id,
+        objectId: userInfo?._id,
+        username: userInfo ? userInfo?.username : user.username,
+        name: userInfo ? userInfo?.name : user.firstName ?? "",
+        bio: userInfo ? userInfo?.bio : "",
+        image: userInfo ? userInfo?.image : user.imageUrl,
+    };
 
-  return (
-    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20 background-image-section'>
-      <h1 className='head-text'>Onboarding</h1>
-      <p className='mt-3 text-base-regular text-da-1'>
-        Complete your profile now, to use Swarm.
-      </p>
+    return (
+        <main className="min-h-screen flex flex-col bg-cover bg-center bg-no-repeat px-10 py-32 background-image-section">
+            <h1 className="head-text">Onboarding</h1>
+            <p className="mt-3 text-base-regular text-da-1">
+                Complete your profile now, to use Swarm.
+            </p>
 
-      <section className='mt-9 p-10 items-center'>
-        <AccountProfile user={userData} btnTitle='Continue' />
-      </section>
-    </main>
-  )
+            <section className="mt-9 p-10 items-center">
+                <AccountProfile user={userData} btnTitle="Continue" />
+            </section>
+        </main>
+    );
 }
 
 export default Page;
